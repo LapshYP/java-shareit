25683eed91edd27ae4f9da56269ea27c77d9bc13
LapshYP <qqqqqqzqqqqqq@yandex.ru>
2023-07-01 18:38:32 +0300

    тз14 на проверку

--- src/main/java/ru/practicum/shareit/item/dto/ItemDTO.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDTO.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item.dto;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
@@ -13,8 +14,9 @@ import javax.validation.constraints.NotNull;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ItemDTO {
     int id;
-    @NotNull
+    @NotBlank
     String name;
+    @NotBlank
     @NotNull
     String description;
     @NotNull
--- src/main/java/ru/practicum/shareit/user/dto/UserDTO.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDTO.java
@@ -4,7 +4,6 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
