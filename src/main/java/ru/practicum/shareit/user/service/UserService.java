40061da8c6459ec7061ba4d0a05da844dad1816b
LapshYP <qqqqqqzqqqqqq@yandex.ru>
2023-07-01 18:22:05 +0300

    тз14 на проверку

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -1,13 +1,10 @@
 package ru.practicum.shareit.user.service;
 
-
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.user.dto.UserDTO;
 
 import java.util.List;
-@Transactional(readOnly = true)
+
 public interface UserService {
-    @Transactional
     UserDTO createUserSerivce(UserDTO userDTO);
 
     List<UserDTO> getAll();
